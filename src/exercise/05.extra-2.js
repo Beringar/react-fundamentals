e232e422670cc3428264b3aeb33baf369927036f
Berenguer Pou <berenguer.pou@gmail.com>
2023-09-23 18:55:34 +0200

    exercise 5 extra 2

---
+++ src/exercise/05.extra-2.js
@@ -0,0 +1,35 @@
+// Styling
+// http://localhost:3000/isolated/exercise/05.js
+
+import * as React from 'react'
+import '../box-styles.css'
+
+const Box = ({size, style, children}) => {
+  const sizeClassName = size ? `box--${size}` : ''
+  return (
+    <div
+      className={`box ${sizeClassName}`}
+      style={{fontStyle: 'italic', ...style}}
+    >
+      {children}
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <Box size="small" style={{backgroundColor: 'lightblue'}}>
+        small lightblue box
+      </Box>
+      <Box size="medium" style={{backgroundColor: 'orange'}}>
+        medium pink box
+      </Box>
+      <Box size="large" style={{backgroundColor: 'pink'}}>
+        large orange box
+      </Box>
+    </div>
+  )
+}
+
+export default App
